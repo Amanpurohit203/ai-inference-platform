88e429e6957bf26ae2048ba60d020cebdd082a27
Amanpurohit <purohitaman203@gmail.com>
2026-07-01 09:04:01 +0000

    added s3 backend

--- infra/main.tf
+++ infra/main.tf
@@ -6,8 +6,18 @@ terraform {
       version = "~> 5.0"
     }
   }
+
+  backend "s3" {
+    bucket  = "ml-inference-platform-tfstate-697015998406"
+    key     = "eks/terraform.tfstate"
+    region  = "ap-south-1"
+    encrypt = true
+  }
 }
 
 provider "aws" {
   region = var.aws_region
 }
+
+
+ 
