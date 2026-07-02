b9b5e95cca8d822d765ebc9a8b3aa721b833ec11
Amanpurohit <92878588+Amanpurohit203@users.noreply.github.com>
2026-07-02 08:00:37 +0530

    Change default project name to 'ai-inference'

--- infra/variable.tf
+++ infra/variable.tf
@@ -7,7 +7,7 @@ variable "aws_region" {
 variable "project_name" {
   description = "Project name used as a prefix for resources"
   type        = string
-  default     = "ml-inference"
+  default     = "ai-inference"
 }
 
 variable "cluster_version" {
