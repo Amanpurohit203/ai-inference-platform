035ac5660abafa6723361653c1628391166fc89d
Amanpurohit <92878588+Amanpurohit203@users.noreply.github.com>
2026-07-02 09:58:47 +0530

    Update vpc.tf

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -6,8 +6,8 @@ module "vpc" {
   cidr = "10.1.0.0/16"
 
   azs             = ["${var.aws_region}a", "${var.aws_region}b"]
-  private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
-  public_subnets  = ["10.0.101.0/24", "10.0.102.0/24"]
+  private_subnets = ["10.1.1.0/24", "10.1.2.0/24"]
+  public_subnets  = ["10.1.101.0/24", "10.1.102.0/24"]
 
   enable_nat_gateway = true
   single_nat_gateway = true # keeps cost down vs one NAT per AZ
