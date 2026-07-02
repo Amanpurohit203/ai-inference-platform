99d98566cfeed20c1cce1794f5692e23a2b712fc
Amanpurohit <92878588+Amanpurohit203@users.noreply.github.com>
2026-07-02 08:16:05 +0530

    Update vpc.tf

--- infra/vpc.tf
+++ infra/vpc.tf
@@ -3,7 +3,7 @@ module "vpc" {
   version = "~> 5.0"
 
   name = "${var.project_name}-vpc"
-  cidr = "10.0.0.0/16"
+  cidr = "10.1.0.0/16"
 
   azs             = ["${var.aws_region}a", "${var.aws_region}b"]
   private_subnets = ["10.0.1.0/24", "10.0.2.0/24"]
